fc591dfb76a39f9430b11af0a02db5cc9743c4d3
kodirov8788 <kodirov8788@gmail.com>
2023-04-03 15:14:05 +0500

    added project

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import './App.css';
 import Banner from "./components/Banner/Banner"
 import Products from "./components/Products/Products"
 import Sidebar from "./components/Saidbar/Saidebar"
+import Lear from "./components/Lear/Lear"
 function App() {
   return (
     <div className="App">
-      <Sidebar />
+
       <Banner />
       <Products />
+      <Sidebar />
+      <Lear />
     </div>
   );
 }
